c6f911b083c9400ad99a66100b176dabb0ea9676
NPCFISH <93332534+TheNPCFISH@users.noreply.github.com>
2022-07-15 15:12:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -87,16 +87,11 @@ for (i = 0; i <= snowMax; i++) {
 window.addEventListener('resize', resize);
 window.addEventListener('load', initSnow);
 
-// Get the input field
 var input = document.getElementById("textbox");
 
-// Execute a function when the user releases a key on the keyboard
 input.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
   if (event.keyCode === 13) {
-    // Cancel the default action, if needed
     event.preventDefault();
-    // Trigger the button element with a click
     document.getElementById("btn").click();
   }
 });
