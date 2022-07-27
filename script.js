149ddbd9590b01237417fcc2f4df7ec7c6151ec2
NPCFISH <93332534+TheNPCFISH@users.noreply.github.com>
2022-07-27 09:51:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,14 +9,15 @@ input.addEventListener("keyup", function(event) {
 function lol(){
 	var text = document.getElementById('textbox').value;
 	if (text.startsWith("https://") == true) {
-	    window.location = text.substring(8);
+	    window.open(text, "_blank");
 	}	
 	if (text.startsWith("http://") == true) {
-	    window.location = text.substring(7);
+	    window.open(text, "_blank");
 	}
 	if (text.startsWith("//") == true) {
-	    window.location = text.substring(2);
+	    window.open(text, "_blank");
         if (text.startsWith("//") == false && text.startsWith("http://") == false && text.startsWith("https://") == false) {
-            window.location = "//" + text;
+            window.open("//" + text, "_blank");
+        }
         }
 }
