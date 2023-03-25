361ebbdb0fd27b4f4faffb28081778e47e1086a0
NPCFISH <93332534+TheNPCFISH@users.noreply.github.com>
2023-03-25 14:41:53 +0200

    Update secret.js

--- secret.js
+++ secret.js
@@ -1,1 +1,83 @@
-var _0xc11691=_0x1c73;(function(_0x1750db,_0x25530a){var _0xbb90dc=_0x1c73,_0x167d9e=_0x1750db();while(!![]){try{var _0x2d7c70=parseInt(_0xbb90dc(0x9e))/0x1*(-parseInt(_0xbb90dc(0xa5))/0x2)+-parseInt(_0xbb90dc(0xa8))/0x3+-parseInt(_0xbb90dc(0xa2))/0x4*(-parseInt(_0xbb90dc(0xa3))/0x5)+parseInt(_0xbb90dc(0x9d))/0x6+parseInt(_0xbb90dc(0x9a))/0x7+-parseInt(_0xbb90dc(0x9b))/0x8*(-parseInt(_0xbb90dc(0x9c))/0x9)+parseInt(_0xbb90dc(0xa9))/0xa*(parseInt(_0xbb90dc(0x98))/0xb);if(_0x2d7c70===_0x25530a)break;else _0x167d9e['push'](_0x167d9e['shift']());}catch(_0x1fbc9a){_0x167d9e['push'](_0x167d9e['shift']());}}}(_0x348c,0xbfdc0));var visits=0x0;function _0x348c(){var _0x4547a5=['71313ITEaXy','error','5107977zprfBL','17024RHzdPo','4527azdwZj','1616910kaUKEW','14xQgjJt','https://cors-anywhere.herokuapp.com/https://apis.roblox.com/datastores/v1/universes/4511607781/standard-datastores/datastore/entries','then','value','3260924LeLFrF','5XqrHmH','POST','167306EPLYEs','catch','json','3347460psDyYl','290pHUAkM','getElementById','application/json'];_0x348c=function(){return _0x4547a5;};return _0x348c();}function _0x1c73(_0x1a7cbb,_0x16c73a){var _0x348c24=_0x348c();return _0x1c73=function(_0x1c7306,_0x4a655d){_0x1c7306=_0x1c7306-0x96;var _0x1b35c9=_0x348c24[_0x1c7306];return _0x1b35c9;},_0x1c73(_0x1a7cbb,_0x16c73a);}fetch('https://cors-anywhere.herokuapp.com/https://apis.roblox.com/datastores/v1/universes/4511607781/standard-datastores/datastore/entries',{'headers':{'x-api-key':'p8ZqnG2uNk2l+VilE3oFUffESm6fAeK2fj7s5wXY9ltA43yN'}})[_0xc11691(0xa0)](_0x589e14=>_0x589e14[_0xc11691(0xa7)]())[_0xc11691(0xa0)](_0x305d6c=>{var _0x5910bb=_0xc11691;console['log'](_0x305d6c),visits=_0x305d6c['data'][0x0][_0x5910bb(0xa1)]['visits']+0x1,document[_0x5910bb(0x96)]('counter')['innerHTML']=visits,fetch(_0x5910bb(0x9f),{'method':_0x5910bb(0xa4),'headers':{'Access-Control-Allow-Origin':'*','Content-Type':_0x5910bb(0x97),'x-api-key':'p8ZqnG2uNk2l+VilE3oFUffESm6fAeK2fj7s5wXY9ltA43yN'},'body':JSON['stringify']({'data':{'visits':visits}})});})[_0xc11691(0xa6)](_0x33d9fa=>{var _0x5777f4=_0xc11691;console[_0x5777f4(0x99)](_0x33d9fa);});
+var F = A;
+function A(Y, n) {
+    var J = u();
+    return A = function (L, I) {
+        L = L - 0x12c;
+        var s = J[L];
+        return s;
+    }, A(Y, n);
+}
+(function (J, L) {
+    var Z = A, I = J();
+    while (!![]) {
+        try {
+            var s = parseInt(Z(0x148)) / 0x1 * (-parseInt(Z(0x12d)) / 0x2) + -parseInt(Z(0x13d)) / 0x3 * (parseInt(Z(0x142)) / 0x4) + parseInt(Z(0x137)) / 0x5 + parseInt(Z(0x141)) / 0x6 + parseInt(Z(0x13e)) / 0x7 * (-parseInt(Z(0x132)) / 0x8) + -parseInt(Z(0x14a)) / 0x9 * (parseInt(Z(0x143)) / 0xa) + -parseInt(Z(0x12e)) / 0xb * (-parseInt(Z(0x136)) / 0xc);
+            if (s === L)
+                break;
+            else
+                I['push'](I['shift']());
+        } catch (z) {
+            I['push'](I['shift']());
+        }
+    }
+}(u, 0x63ab7));
+function u() {
+    var X = [
+        'error',
+        'p8ZqnG2uNk2l+VilE3oFUffESm6fAeK2fj7s5wXY9ltA43yN',
+        'https://mysnow.org',
+        '4662024jUVwoq',
+        'headers',
+        'https://example.com',
+        'data',
+        '30329808RnpSUW',
+        '270705BocdxX',
+        'Content-Type',
+        'counter',
+        'log',
+        'value',
+        'json',
+        '9nJNUdM',
+        '7OyPPYu',
+        'Origin',
+        'stringify',
+        '564408ornUmF',
+        '969336DGFQPV',
+        '1160NRKhFZ',
+        'catch',
+        'visits',
+        'then',
+        'GET',
+        '31ZCoGeC',
+        'http://cors-anywhere.herokuapp.com/https://apis.roblox.com/datastores/v1/universes/4511607781/standard-datastores/datastore/entries',
+        '53280LVCRDb',
+        'method',
+        'x-api-key',
+        '17482OyICwT',
+        '11IwhTfm'
+    ];
+    u = function () {
+        return X;
+    };
+    return u();
+}
+var visits = 0x0, Y = {};
+Y[F(0x13f)] = F(0x134), Y[F(0x12c)] = F(0x130);
+var n = {};
+n[F(0x14b)] = F(0x147), n[F(0x133)] = Y, fetch(F(0x149), n)[F(0x146)](J => J[F(0x13c)]())[F(0x146)](s => {
+    var g = F;
+    console[g(0x13a)](s), visits = s[g(0x135)][0x0][g(0x13b)][g(0x145)] + 0x1, document['getElementById'](g(0x139))['innerHTML'] = visits;
+    var z = {};
+    z[g(0x138)] = 'application/json', z[g(0x13f)] = g(0x131), z['x-api-key'] = 'p8ZqnG2uNk2l+VilE3oFUffESm6fAeK2fj7s5wXY9ltA43yN';
+    var Q = {};
+    Q[g(0x145)] = visits;
+    var G = {};
+    G[g(0x135)] = Q, fetch(g(0x149), {
+        'method': 'POST',
+        'headers': z,
+        'body': JSON[g(0x140)](G)
+    });
+})[F(0x144)](J => {
+    var H = F;
+    console[H(0x12f)](J);
+});
